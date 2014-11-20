ac3df2d5c106de429c12d991784046796ec0371c
Mitchell Bagley <bagleymj@gmail.com>
2014-11-20 17:50:08 -0500

    Added Show View

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,18 @@
+class HousesController < ApplicationController
+  def index
+					@houses = House.all
+  end
+
+  def show
+					@house = House.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
