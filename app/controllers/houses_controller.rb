4c9f9dd352589a342e3502a85d89c1d08a552acb
Mitchell Bagley <bagleymj@gmail.com>
2014-11-20 18:30:28 -0500

    Added Ability to Add New Houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,11 +8,24 @@ class HousesController < ApplicationController
   end
 
   def new
+					@house = House.new
   end
 
-  def edit
+  def create
+					@house = House.new(params[:house])
+					if @house.save
+									redirect_to houses_path
+					else
+									render :new
+					end
   end
+
+	def edit
+	end
 
   def delete
   end
+
+	def house_params
+	end
 end
