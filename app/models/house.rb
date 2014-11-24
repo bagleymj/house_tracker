2b551c9640bf75f312184d7027d6f85d1028625b
Mitchell Bagley <bagleymj@gmail.com>
2014-11-24 18:57:36 -0500

    Added total score to index

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,12 +3,13 @@ class House < ActiveRecord::Base
 
   has_many :scores
 
-  #def totalscore(@house)
-  #  totaled_score = 0
-  #  @house.scores.each do |score|
-  #    totaled_score = totaled_score + score.score
-  #  end
-  #  return totaled_score
-  #end
+  def totalscore(id)
+    totaled_score = 0
+    sum_house = House.find(id)
+    sum_house.scores.each do |score|
+      totaled_score = totaled_score + score.score
+    end
+    return totaled_score
+  end
 
 end
